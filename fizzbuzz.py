743c1c6e4c0806898433b8fd84b46dd3b2a72ada
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-04-25 16:59:06 -0400

    quick fizzbuzz, April 25

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+# April 25, 2018
+# Starting off simple because of finals week
+
+for i in range(101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(str(i))
