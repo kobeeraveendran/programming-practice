18caa978d932709f420e676643c6356d04aa0f45
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-04-28 22:00:21 -0400

    finished basic functionality, April 28

--- anagrams_scratch.java
+++ anagrams_scratch.java
@@ -2,21 +2,40 @@ public class anagrams_scratch
 {
     public static void main(String[] args)
     {
-        System.out.println("Clint Eastwood and Old West Action: ");
+        System.out.println("Clint Eastwood and Old West Action: " + anagrams("Clint Eastwood", "Old West Action"));
+        System.out.println("Dog and God: " + anagrams("dog", "God"));
+        System.out.println("Public relations and crap built on lies: " + anagrams("Public relations", "crap built on lies"));
+        System.out.println("Racecar and racecars: " + anagrams("Racecar", "racecars"));
     }
 
-    public static boolean anagrams(String string1, String strin2)
+    public static boolean anagrams(String string1, String string2)
     {
-        // sort the two strings manually
+        // count letter frequencies of both strings, then match based on that
+        string1 = string1.toLowerCase().replace(" ", "");
+        string2 = string2.toLowerCase().replace(" ", "");
+
         if(string1.length() != string2.length())
         {
             return false;
         }
 
-        // count letter frequencies of both strings, then match based on that
-        countFlag = false;
-        char[] charTable = new char[26];
+        boolean countFlag = true;
+        int[] charFreqs1 = new int[26];
+        int[] charFreqs2 = new int[26];
         
+        for(int i = 0; i < string1.length(); i++)
+        {
+            charFreqs1[string1.charAt(i) - 'a'] += 1;
+            charFreqs2[string2.charAt(i) - 'a'] += 1;
+        }
+
+        // check if the frequencies match
+        for(int i = 0; i < 26; i++) {
+            if(charFreqs1[i] != charFreqs2[i])
+            {
+                countFlag = false;
+            }
+        }
 
         return countFlag;
     }
