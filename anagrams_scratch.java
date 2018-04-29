101d3b00c42f0d60b5cedc50fa61d55d56bb2752
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-04-28 21:42:19 -0400

    initial commit, April 28

---
+++ anagrams_scratch.java
@@ -0,0 +1,23 @@
+public class anagrams_scratch
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Clint Eastwood and Old West Action: ");
+    }
+
+    public static boolean anagrams(String string1, String strin2)
+    {
+        // sort the two strings manually
+        if(string1.length() != string2.length())
+        {
+            return false;
+        }
+
+        // count letter frequencies of both strings, then match based on that
+        countFlag = false;
+        char[] charTable = new char[26];
+        
+
+        return countFlag;
+    }
+}
