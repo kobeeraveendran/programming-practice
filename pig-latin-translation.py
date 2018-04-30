ba971b7f7dbe8de0c3d4ef9c418c2a8c7de08389
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-04-30 16:50:52 -0400

    now translates from pig latin back to English, with initial uppercase support

--- pig-latin-translation.py
+++ pig-latin-translation.py
@@ -23,4 +23,29 @@ def translate_to_pig(english):
 def translate_to_eng(piglatin):
     stringlist = piglatin.split(' ')
 
+    newstringlist = []
+
+    first = True
+    for string in stringlist:
+        templist = list(string)
+        del templist[len(templist) - 1]
+        del templist[len(templist) - 1]
+        tempchar = templist[len(templist) - 1]
+        templist.remove(tempchar)
+        if first:
+            templist.insert(0, tempchar.upper())
+            templist[1] = templist[1].lower()
+            first = False
+        else:
+            templist.insert(0, tempchar)
+        newstringlist.append(''.join(templist))
+        newstringlist.append(' ')
+
+    retval = ''.join(newstringlist)
+
+    return retval
+
+
 print("Translate 'The quick brown fox' to Pig Latin: " + translate_to_pig('The quick brown fox'))
+
+print("Translate 'Hetay uickqay rownbay oxfay' to English: " + translate_to_eng('Hetay uickqay rownbay oxfay'))
