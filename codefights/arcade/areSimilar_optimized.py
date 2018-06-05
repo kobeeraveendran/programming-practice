dae9602a54ba32af2db2ac23b90b03f3a1bc3f41
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-06-04 20:50:29 -0400

    added test cases and changed order of similarity checks to eliminate unnecessary  computation

--- codefights/arcade/areSimilar_optimized.py
+++ codefights/arcade/areSimilar_optimized.py
@@ -11,6 +11,26 @@ def areSimilar(a, b):
             a_diff.append(a[i])
             b_diff.append(b[i])
     
+    if len(a_diff) > 2:
+        return False
+
     a_diff.reverse()
 
-    return len(a_diff) == 2 and str(a_diff) == str(b_diff)
+    return str(a_diff) == str(b_diff)
+
+test1a = [1, 2, 3]
+test1b = [1, 2, 3]
+
+test2a = [1, 2, 3]
+test2b = [2, 1, 3]
+
+test3a = [1, 2, 2]
+test3b = [2, 1, 1]
+
+test4a = [4, 6, 3]
+test4b = [3, 4, 6]
+
+print('Test 1: Expected: True' + ' Output: ' + str(areSimilar(test1a, test1b)))
+print('Test 2: Expected: True' + ' Output: ' + str(areSimilar(test2a, test2b)))
+print('Test 3: Expected: False' + ' Output: ' + str(areSimilar(test3a, test3b)))
+print('Test 4: Expected: False' + ' Output: ' + str(areSimilar(test4a, test4b)))
