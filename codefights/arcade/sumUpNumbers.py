aac56b2f5fa58337a66e6322557861e64eb637f4
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-06-09 12:46:38 -0400

    removed redundancy for elegance

--- codefights/arcade/sumUpNumbers.py
+++ codefights/arcade/sumUpNumbers.py
@@ -3,4 +3,4 @@ import re
 def sumUpNumbers(inputString):
     parsed = re.findall('\d+', inputString)
 
-    return 0 if len(parsed) == 0 else sum(map(int, parsed))
+    return sum(map(int, parsed))
