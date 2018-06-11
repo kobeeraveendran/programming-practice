95210bc11e2264e7d89a79cffc3f2c31030113b5
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-06-11 01:12:05 -0400

    fully functional version of firstNotRpeatingCharacter

--- codefights/arrays/firstNotRepeatingCharacter.py
+++ codefights/arrays/firstNotRepeatingCharacter.py
@@ -1,6 +1,18 @@
 def firstNotRepeatingCharacter(s):
+    index_track = {}
+
     for i in range(len(s)):
-        if s.count(s[i]) == 1:
-            return s[i]
+        if s[i] in index_track:
+            index_track[s[i]] = -1
+        else:
+            index_track[s[i]] = i
+
+    min_index = len(s)
+    retval = '_'
+
+    for key in index_track:
+        if index_track[key] > -1:
+            min_index = min(index_track[key], min_index)
+            retval = s[min_index]
     
-    return '_'
+    return retval
