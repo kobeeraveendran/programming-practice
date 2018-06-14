0613a03eff2a99df69edf18b18813425ea4889b9
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-06-14 18:42:25 -0400

    complete version of Uber's perfectCity challenge

--- codefights/company-challenges/perfectCity.py
+++ codefights/company-challenges/perfectCity.py
@@ -3,19 +3,17 @@ def perfectCity(departure, destination):
     path_length = 0
     nearest_start_x = math.ceil(departure[0]) if departure[0] < destination[0] else math.floor(departure[0])
     nearest_start_y = math.ceil(departure[1]) if departure[1] < destination[1] else math.floor(departure[1])
-    nearest_end_x = math.ceil(destination[0]) if destination[0] > departure[0] else math.floor(destination[0])
-    nearest_end_y = math.ceil(destination[1]) if destination[1] > departure[1] else math.floor(destination[1])
+    nearest_end_x = math.floor(destination[0]) if destination[0] > nearest_start_x else math.ceil(destination[0])
+    nearest_end_y = math.floor(destination[1]) if destination[1] > nearest_start_y else math.ceil(destination[1])
 
     delta_x_start = abs(nearest_start_x - departure[0])
     delta_y_start = abs(nearest_start_y - departure[1])
     delta_x_end = abs(nearest_end_x - destination[0])
     delta_y_end = abs(nearest_end_y - destination[1])
 
-    euclidian_distance = abs(nearest_end_x - nearest_start_x) + abs(nearest_end_y - nearest_start_y)
+    #print(delta_x_start, delta_y_start, delta_x_end, delta_y_end)
 
-    #print(nearest_start_x, nearest_start_y)
-    #print(nearest_end_x, nearest_end_y)
-    #print(euclidian_distance)
+    euclidian_distance = abs(nearest_end_x - nearest_start_x) + abs(nearest_end_y - nearest_start_y)
 
     path_length = delta_x_start + delta_y_start + delta_x_end + delta_y_end + euclidian_distance
 
@@ -24,3 +22,7 @@ def perfectCity(departure, destination):
 # should be 2.7
 print(perfectCity([0.4, 1], [0.9, 3]))
 print(perfectCity([0.5, 0.5], [1.5, 1.5]))
+print(perfectCity([0.8, 1], [0.1, 0.1]))
+print(perfectCity([0, 0], [0, 0]))
+print(perfectCity([0, 1], [0, 2]))
+print(perfectCity([1.2, 4.5], [0, 1]))
