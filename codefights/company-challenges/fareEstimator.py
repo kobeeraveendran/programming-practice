7cfdf89a0a948f2cdf641056c64395a5e7b9c63e
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-06-16 15:13:57 -0400

    shorter solution to Uber's fareEstimator challenge

--- codefights/company-challenges/fareEstimator.py
+++ codefights/company-challenges/fareEstimator.py
@@ -1,3 +1,6 @@
 import numpy as np
 def fareEstimator(ride_time, ride_distance, cost_per_minute, cost_per_mile):
     return np.add((ride_time * np.array(cost_per_minute)), (ride_distance * np.array(cost_per_mile)))
+
+# test 1
+print(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]))
