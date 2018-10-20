e1a4a02bfa048e35aa68fa942d4510a572d0ebe1
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-10-20 15:25:12 -0400

    added better solution (runtime-wise) for missing number; works for any number of numbers missing

--- interview-run/arrays/missing_number_improved.py
+++ interview-run/arrays/missing_number_improved.py
@@ -13,20 +13,40 @@ def missingNumberImprovedNaive(numberList, maxRange):
 
     return missing
 
+def missingNumberImprovedBetter(numberList, maxRange):
+
+    maxRange -= 1
+
+    numberList.sort()
+
+    missing = []
+
+    i = 1
+    j = 0
+
+    while i != maxRange:
+        if numberList[j] != i:
+            i += 1
+            missing.append(numberList[j] - 1)
+
+        i += 1
+        j += 1
+
+    return missing
 
 
 tc1 = [x for x in range(1, 100)]
 tc1.remove(57)
 tc1.remove(25)
 
-print(missingNumberImproved(tc1, 100))
+print(missingNumberImprovedBetter(tc1, 100))
 
 tc2 = [x for x in range(1, 100)]
 tc2.remove(52)
 tc2.remove(89)
-print(missingNumberImproved(tc2, 100))
+print(missingNumberImprovedBetter(tc2, 100))
 
 tc3 = [x for x in range(1, 200) if x != 109 or x != 21]
 tc3.remove(109)
 tc3.remove(21)
-print(missingNumberImproved(tc3, 200))
+print(missingNumberImprovedBetter(tc3, 200))
