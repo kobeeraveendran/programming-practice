ae11cbaa2db024cd2b4a8335df2873681ee0095e
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-10-20 13:49:47 -0400

    dfs initial commit

---
+++ interview-run/dfs.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def dfs(self, start):
+        
