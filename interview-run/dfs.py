123e56364165aa9908f81e4714b1128647eb034f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-10-20 13:22:49 -0400

    added bfs soluton

--- interview-run/dfs.py
+++ interview-run/dfs.py
@@ -1,28 +1,32 @@
-#from collections import defaultdict
+from collections import defaultdict
 
 class Graph:
 
     def __init__(self):
-        self.graph = {}
+        self.graph = defaultdict(list)
 
-    def addEdge(parent, child):
-        if graph[parent]:
-            graph[parent].append(child)
-        
-        else:
-            graph[parent] = [child]
+    def addEdge(self, parent, child):
 
-    def bfs(start):
+        self.graph[parent].append(child)
+
+    def bfs(self, start):
         queue = []
+
+        visited = [False for x in range(len(self.graph))]
 
         queue.append(start)
 
+        visited[start] = True
+
         while queue:
-            curr = queue.pop()
+            curr = queue.pop(0)
 
-            print(curr + ' ')
+            print(curr)
 
-            queue.append(self.graph[curr])
+            for child in self.graph[curr]:
+                if not visited[child]:
+                    queue.append(child)
+                    visited[child] = True
 
 graph = Graph()
 graph.addEdge(0, 1)
@@ -34,3 +38,12 @@ graph.addEdge(3, 3)
 
 print('BFS traversal of graph:')
 graph.bfs(2)
+
+print('BFS traversal of graph:')
+graph.bfs(0)
+
+print('BFS traversal of graph:')
+graph.bfs(1)
+
+print('BFS traversal of graph:')
+graph.bfs(3)
